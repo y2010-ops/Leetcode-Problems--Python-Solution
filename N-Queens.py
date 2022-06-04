4482f5e2889bdf2a64d7a5c3c5cdd6cf23e45687
Yamini Vijaywargiya <59636994+y2010-ops@users.noreply.github.com>
2022-06-04 10:39:53 +0530

    Add files via upload

---
+++ N-Queens.py
@@ -0,0 +1,37 @@
+class Solution:
+    def solveNQueens(self, n: int):
+        col = set()
+        posDiag = set()
+        negDiag = set()
+
+        result = []
+        board = [["."] * n for i in range(n)]
+
+        def backtrack(r):
+            if r == n:
+                copy = ["".join(row) for row in board]
+                result.append(copy)
+                return
+
+            for c in range(n):
+                if c in col or (r + c) in posDiag or (r - c) in negDiag:
+                    continue
+
+                col.add(c)
+                posDiag.add(r + c)
+                negDiag.add(r - c)
+                board[r][c] = "Q"
+
+                backtrack(r + 1)
+
+                col.remove(c)
+                posDiag.remove(r + c)
+                negDiag.remove(r - c)
+                board[r][c] = "."
+
+        backtrack(0)
+        return result
+
+p = Solution()
+print(p.solveNQueens(4))
+
