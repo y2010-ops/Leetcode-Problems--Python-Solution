a3836846ab8cd8497d284086b70fb1f2df840ebd
Yamini Vijaywargiya <59636994+y2010-ops@users.noreply.github.com>
2022-06-15 22:06:44 +0530

    Add files via upload

---
+++ Longest String Chain.py
@@ -0,0 +1,21 @@
+class Solution:
+    def longestStrChain(self, words: [str]):
+
+        words.sort(key=len)
+        dic = {}
+
+        for i in words:
+            dic[i] = 1
+
+            for j in range(len(i)):
+
+                # creating words by deleting a letter
+                successor = i[:j] + i[j + 1:]
+                if successor in dic:
+                    dic[i] = max(dic[i], 1 + dic[successor])
+
+        res = max(dic.values())
+        return res
+
+s =Solution()
+print(s.longestStrChain(["a","b","ba","bca","bda","bdca"]))
