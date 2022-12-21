d24537d08c3d12c19a10d9836e58d400ff3aeaef
Yamini Vijaywargiya <59636994+y2010-ops@users.noreply.github.com>
2022-12-21 13:35:05 +0530

    Add files via upload

---
+++ 739 Daily Temperature.py
@@ -0,0 +1,24 @@
+nums = [73,74,75,71,69,72,76,73]
+# Output -> answer =  [1,1,4,2,1,1,0,0]
+
+# # Brute Force
+# res = [0] * len(nums)
+# for i in range(len(nums)):
+#     for j in range(i, len(nums)):
+#         if nums[j] > nums[i]:
+#             res[i] = j - i
+#             break
+# print(res)
+
+# By Using Stack
+ans = [0] * len(nums)
+stack = [] # [[index, temp]]
+
+for i, t in enumerate(nums):
+    while stack and t > stack[-1][1]:
+        index, temp = stack.pop()
+        ans[index] = i - index
+    stack.append([i,t])
+print(stack)
+print(ans)
+
