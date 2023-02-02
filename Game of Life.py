5e5d1a3bca4ace63685d3d0429dfd1141f392ed1
Yamini Vijaywargiya <59636994+y2010-ops@users.noreply.github.com>
2023-02-02 08:32:37 +0530

    Add files via upload

---
+++ Game of Life.py
@@ -0,0 +1,31 @@
+board = [[0, 1, 0], [0, 0, 1], [1, 1, 1], [0, 0, 0]]
+# Output: [[0,0,0],[1,0,1],[0,1,1],[0,1,0]]
+N = len(board)
+M = len(board[0])
+
+places = [(0, 1), (0, -1), (-1, 0), (1, 0), (-1, -1), (-1, 1), (1, -1), (1, 1)]
+list = []
+
+for r in range(N):
+    for c in range(M):
+        count = 0
+        for x, y in places:
+            if 0 <= r + x < N and 0 <= c + y < M and abs(board[r + x][c + y]) == 1:
+                count += 1
+        if board[r][c] == 1:
+            # That means its a Live Cell
+            if count < 2 or count > 3:
+                board[r][c] = -1
+
+        elif board[r][c] == 0:
+            if count == 3:
+                board[r][c] = 2
+
+for r in range(N):
+    for c in range(M):
+        if board[r][c] == -1:
+            board[r][c] = 0
+        elif board[r][c] == 2:
+            board[r][c] = 1
+
+print(board)
