105c02e80fb2be0c16ef54866664ad04158914ea
Yamini Vijaywargiya <59636994+y2010-ops@users.noreply.github.com>
2024-05-12 16:49:54 +0530

    Add files via upload

--- Longest Common Subsequence.py
+++ Longest Common Subsequence.py
@@ -1,13 +1,14 @@
-class Solution:
-    def longestCommonSubsequence(self, text1: str, text2: str):
-        dp = [[0 for i in range(len(text2)+1)] for j in range(len(text1)+1)]
-        for j in range(len(text1)-1,-1, -1):
-            for i in range(len(text2)-1, -1, -1):
-                if text1[j] == text2[i]:
-                    dp[j][i] = 1 + dp[j+1][i+1]
-                else:
-                    dp[j][i] = max(dp[j][i+1], dp[j+1][i])
-        return dp[0][0]
+text1 = "ezupkr"
+text2 = "ubmrapg"
+arr = [[0 for i in range(len(text2)+1)] for j in range(len(text1)+1)]
+print(arr)
 
-s = Solution()
-print(s.longestCommonSubsequence("Yamini", "Vijaywargiya"))
+for r in range(len(text1)-1, -1, -1):
+    for c in range(len(text2)-1, -1, -1):
+        if text1[r] == text2[c]:
+            arr[r][c] = 1 + arr[r+1][c+1]
+        else:
+            arr[r][c] = max(arr[r+1][c], arr[r][c+1])
+print(arr)
+print(arr[0][0])
+
